b42ad860f1f5e2e203c45d2983b9b3951739a0ae
Willian Lins Araújo <williannlins@gmail.com>
2024-05-22 16:18:44 -0300

    storybook

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa laboriosam atque nobis cum blanditiis sequi id est officia mollitia impedit incidunt, suscipit cumque repellat expedita optio aspernatur, ullam possimus nemo.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -1,6 +1,6 @@
 export const colors = {
-  white: '#FFF',
   black: '#000',
+  white: '#FFF',
 
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
@@ -15,6 +15,4 @@ export const colors = {
   ignite500: '#00875F',
   ignite700: '#015F43',
   ignite900: '#00291D',
-
-  test: '#fff',
 }
